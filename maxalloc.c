0d7bc15bcb36c823d10c2b3f91f5240e116bf034
Laura Toma <ltoma@bowdoin.edu>
2017-09-04 12:36:46 -0400

    Create maxalloc.c

---
+++ maxalloc.c
@@ -0,0 +1,17 @@
+/* 
+
+maxalloc.c 
+
+your name, date 
+
+This program  lets you find out the maximum size of contiguous memory you can allocate.
+
+*/
+
+
+int main() {
+
+  long maxsize = 0; 
+  
+  printf("maxsize=%ld\n", maxsize);
+}
