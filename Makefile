81bef6c87abc1995433cc1f99ab2d8693390847a
lauratoma <ltoma@bowdoin.edu>
2017-09-04 21:22:55 -0400

    tabs got messed up

--- Makefile
+++ Makefile
@@ -2,7 +2,7 @@ CC = gcc
 CFLAGS = -Wall
 
 maxalloc: maxalloc.c
-    $(CC) $(CFLAGS) -o $@ maxalloc.c
+	$(CC) $(CFLAGS) -o $@ maxalloc.c
 
 clean:
-    rm -f maxalloc
+	rm -f maxalloc
--- maxalloc.c
+++ maxalloc.c
@@ -4,9 +4,12 @@ maxalloc.c
 
 your name, date 
 
-This program  lets you find out the maximum size of contiguous memory you can allocate.
+This program lets you find out the maximum size of contiguous memory
+you can allocate.
 
 */
+
+#include <stdio.h> 
 
 
 int main() {
